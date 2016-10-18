85f0ecd544b2d45ff78605687b106ed7164f4cd5
edzima <fachowosc@gmail.com>
2016-10-18 20:01:42 +0200

    Url menager

--- frontend/views/task/create.php
+++ frontend/views/task/create.php
@@ -7,8 +7,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Task */
 
-$this->title = 'Create Task';
-$this->params['breadcrumbs'][] = ['label' => 'Tasks', 'url' => ['index']];
+$this->title = 'Nowe spotkanie';
+$this->params['breadcrumbs'][] = ['label' => 'Spotkania', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="task-create">
