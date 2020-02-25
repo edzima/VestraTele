ae6548a9d9e236f2da0a6f86848ddf8beee037d1
Artur <atipezda@gmail.com>
2020-02-25 21:12:09 +0100

    building update method

--- frontend/vue/.eslintrc.js
+++ frontend/vue/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/no-var-requires': 0,
-    "@typescript-eslint/no-explicit-any" : 0
+    "@typescript-eslint/no-explicit-any" : 0,
+    "@typescript-eslint/camelcase": 'warn'
   }
 }
--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -14,17 +14,20 @@
       :maxTime="calendar.maxTime"
       :eventDurationEditable="calendar.eventDurationEditable"
       :columnHeaderFormat="calendar.columnHeaderFormat"
+      @eventDrop="handleChangeDates"
     />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import listWeekPlugin from '@fullcalendar/list'
+import plLang from '@fullcalendar/core/locales/pl'
 import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
-// const FullCalendar = require('@fullcalendar/vue').default
+import axios from 'axios'
+const Calendar = require('@fullcalendar/core')
 const FullCalendar = require('@fullcalendar/vue').default
 
 @Component({
@@ -33,28 +36,108 @@ const FullCalendar = require('@fullcalendar/vue').default
   }
 })
 export default class App extends Vue {
+  @Prop({
+    default: () => true,
+    required: false
+  })
+  private allowUpdate!: boolean; // allow user to edit events
+
+  @Prop({
+    default: () => 21,
+    required: false
+  })
+  private agentId!: number;
+
+  @Prop({
+    default: () => '/update',
+    required: false
+    // -----PARAMS-----
+    // id - eventId
+    // date_at - date from
+    // date_end_at - date to
+  })
+  private URLUpdate!: string;
+
+  @Prop({
+    default: () => '/list',
+    // -----PARAMS-----
+    // dateFrom
+    // dateTo
+    // agentId
+    required: false
+  })
+  private URLGetCalendar!: string;
+
   private calendar: any = {
-    plugins: [
-      dayGridPlugin,
-      listWeekPlugin,
-      timeGridPlugin,
-      interactionPlugin
-    ],
+    plugins: [dayGridPlugin, listWeekPlugin, timeGridPlugin, interactionPlugin],
     header: {
       left: 'title',
-      center: 'today prev,next'
-      // right: 'dayGridMonth,timeGridWeek,dayGridDay'
+      center: 'today prev,next',
+      right: 'dayGridMonth,timeGridWeek,dayGridDay'
     },
     defaultView: 'timeGridWeek',
-    // locale: plLang,
-    editable: true,
+    locale: plLang,
+    editable: this.allowUpdate, // FLAG FROM PROPS
     droppable: false, // external calendars events
     minTime: '8:00:00',
     maxTime: '24:00:00',
-    eventDurationEditable: false, // allow to extend time
+    eventDurationEditable: this.allowUpdate, // allow to extend time
     columnHeaderFormat: { weekday: 'long', day: 'numeric' },
-    // businessHours: { start: "8:00", end: "20:00" },
     events: []
+  };
+
+  private async handleChangeDates (e: any): Promise<void> {
+    if (!this.allowUpdate) return // cancel if no permissions
+    const eventCard: any = e.event
+    console.log(eventCard)
+    const parseSettings = {
+      month: 'numeric',
+      year: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false,
+      meridiem: false,
+      separator: '-'
+    }
+    const dateFrom: string = Calendar.formatDate(eventCard.start.toISOString(), parseSettings)
+    const dateTo: string = eventCard.end ? Calendar.formatDate(eventCard.end.toISOString(), parseSettings) : ''
+    const eventId: number = eventCard.id
+
+    const params: any = new URLSearchParams()
+    params.append('id', eventId)
+    params.append('date_at', dateFrom)
+    params.append('date_end_at', dateTo)
+
+    const newEventProperties = {
+      id: eventId,
+      date_at: dateFrom,
+      date_end_at: dateTo
+    }
+
+    axios.post(this.URLUpdate, newEventProperties)
+  }
+
+  private async fetchEvents (): Promise<void> {
+    const res = await axios.get(this.URLGetCalendar, {
+      params: {
+        agentId: this.agentId
+      }
+    })
+    const events = res.data.data
+    const fullCalendarEvents = events.map(eventCard => ({
+      id: eventCard.id,
+      title: eventCard.client,
+      start: eventCard.date_at
+    }))
+    console.log(fullCalendarEvents)
+
+    this.calendar.events = fullCalendarEvents
+  }
+
+  created () {
+    this.fetchEvents()
   }
 }
 </script>
