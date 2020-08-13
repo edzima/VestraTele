013407fc2280e1361892727db28c6ef862b341ef
Artur <atipezda@gmail.com>
2020-08-13 22:52:54 +0200

    added env merging + template for tests environment

--- docker-init.sh
+++ docker-init.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+
+OUTPUT_FILE=".env"
+
+function mergeEnvs() {
+   echo -n "" > $OUTPUT_FILE;
+
+   arr=("$@")
+   for i in "${arr[@]}";
+      do
+          cat "$i" >> $OUTPUT_FILE;
+      done
+
+    # shellcheck disable=SC2145
+    echo "merged ${arr[@]}"
+}
+
+
 echo "******************************************"
 echo "What docker deployment version would you like to start ? "
 options=("Development ====> YII SERVE(frontend) + YII SERVE(backend) + NGNX(reverse proxy) + MYSQL" "Production ====> APACHE(frontend)+ APACHE(backend) + NGNX(reverse proxy) + MYSQL" "Remove containers" "Quit")
@@ -7,14 +25,26 @@ do
     case $opt in
         "Development ====> YII SERVE(frontend) + YII SERVE(backend) + NGNX(reverse proxy) + MYSQL")
             echo "STARTING development environment ..."
-            docker-compose -f docker-compose-development.yml up
+            toMerge=("environments/base.env" "environments/development.env");
+            mergeEnvs "${toMerge[@]}"
+#            docker-compose -f docker-compose-development.yml up
             exit;
             ;;
         "Production ====> APACHE(frontend)+ APACHE(backend) + NGNX(reverse proxy) + MYSQL")
             echo "STARTING production environment ..."
-            docker-compose -f docker-compose-production.yml up
+            toMerge=("environments/base.env" "environments/production.env");
+            mergeEnvs "${toMerge[@]}"
+#            docker-compose -f docker-compose-production.yml up
             exit;
             ;;
+
+        "Tests =====> PHP(YII) + MYSQL")
+#            docker-compose -f docker-compose-tests.yml up
+            exit;
+        ;;
+        "stop all docker containers")
+            exit;
+        ;;
         "Remove containers")
             echo "Removing containers"
             docker-compose -f docker-compose-development.yml down
