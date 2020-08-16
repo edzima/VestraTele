b249c1190b436adc9130e71daee72ebf16d4c5d1
Artur <atipezda@gmail.com>
2020-08-16 19:14:04 +0200

    production docker fix, Auto create db for prod, Storage container included and tested

--- docker/production/sql/sql.sh
+++ docker/production/sql/sql.sh
@@ -2,19 +2,9 @@ echo '*****************************'
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e \
 "
-CREATE USER '$DB_USERNAME'@'frontend' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO '$DB_USERNAME'@'frontend';
+CREATE USER '$DB_USERNAME'@'%' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO '$DB_USERNAME'@'%';
 FLUSH PRIVILEGES;
 " && \
-echo "Successfully created frontend user"
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e \
-"
-CREATE USER '$DB_USERNAME'@'backend' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO '$DB_USERNAME'@'backend';
-FLUSH PRIVILEGES;
-" && \
-echo "Successfully created backend user"
-
-
+echo "Successfully created user"
 echo '*****************************'
