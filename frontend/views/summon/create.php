45d1e78e47cc1c72a8982cc7487c9294a618522a
Artur <atipezda@gmail.com>
2020-09-13 18:46:01 +0200

    Created: Summon => migration, model, search, controller, CRUD

---
+++ frontend/views/summon/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\issue\Summon */
+
+$this->title = 'Create Summon';
+$this->params['breadcrumbs'][] = ['label' => 'Summons', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="summon-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
