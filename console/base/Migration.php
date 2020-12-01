004d8af9998ab06a7e7e0d45e4a3706eaed19eee
edzima <fachowosc@gmail.com>
2020-12-01 10:20:54 +0100

    chore: add convertTimestampToDatetime to migration class.

--- console/base/Migration.php
+++ console/base/Migration.php
@@ -8,6 +8,7 @@
 
 namespace console\base;
 
+use yii\db\Expression;
 use yii\db\Migration as BaseMigration;
 
 class Migration extends BaseMigration {
@@ -17,5 +18,21 @@ class Migration extends BaseMigration {
 			$options = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
 		}
 		parent::createTable($table, $columns, $options);
+	}
+
+	protected function convertTimestampToDatetime(string $table, string $column, string $newColumn = null) {
+		if ($newColumn === null) {
+			$newColumn = $column . '_temp';
+		}
+		$this->addColumn($table, $newColumn, $this->date());
+		$this->update($table, [$newColumn => null], [$newColumn => '1970-01-01']);
+
+		$this->update($table, [$newColumn => new Expression('FROM_UNIXTIME(UNIX_TIMESTAMP(' . $column . '))')]);
+		if ($newColumn === $column . '_temp') {
+			$this->dropColumn($table, $column);
+			$this->renameColumn($table, $newColumn, $column);
+		} else {
+			$this->dropColumn($table, $column);
+		}
 	}
 }
