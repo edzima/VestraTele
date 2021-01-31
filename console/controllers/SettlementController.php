76bd3ae333cb241e31ea18a7aafe3487d1cd8435
edzima <fachowosc@gmail.com>
2021-01-31 09:12:21 +0100

    feat: add console action to check administrative settlements has costs.

---
+++ console/controllers/SettlementController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace console\controllers;
+
+use backend\modules\settlement\models\AdministrativeCalculationForm;
+use common\models\issue\IssuePayCalculation;
+use Yii;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class SettlementController extends Controller {
+
+	public function actionAdministrativeCosts(): void {
+		$models = IssuePayCalculation::find()
+			->andWhere(['type' => IssuePayCalculation::TYPE_ADMINISTRATIVE])
+			->all();
+
+		Console::output('Find administrative settlements: ' . count($models));
+		foreach ($models as $model) {
+			if ($model->hasCosts) {
+				Console::output('Has already costs: ' . $model->getIssueName());
+				Console::output('Costs sum with VAT: ' . $model->getCostsSum(true));
+			} else {
+				Console::output('Settlement: ' . $model->id . ' for Issue: ' . $model->getIssueName() . ' has not costs.');
+				$administrative = AdministrativeCalculationForm::createFromModel($model);
+				$administrative->save();
+				Yii::$app->provisions->removeForPays($model->getPays()->getIds());
+			}
+		}
+	}
+}
