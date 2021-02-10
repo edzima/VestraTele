6958be19945c49431970cf5c46d74d55673a7a5c
arturpezda <atipezda@gmail.com>
2021-02-10 06:41:03 +0100

    FEAT: Calendar development mode index.html, bundle opts.

--- frontend/vue/vue.config.js
+++ frontend/vue/vue.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-	outputDir: '../web/static/calendar',
-	devServer: {
-		proxy: 'https://test.vestra.hekko24.pl'
-	},
-	filenameHashing: false,
+    outputDir: '../web/static/calendar',
+    devServer: {
+        proxy: 'https://test.vestra.hekko24.pl'
+    },
+    filenameHashing: false,
+    chainWebpack: config => {
+        if (process.env.NODE_ENV === 'development') {
+            config
+            .plugin('html')
+            .tap(args => {
+                //  args[0] contains the plugin's options object
+                args[0].template = 'public/index-development.html'
+                return args
+            });
+
+        }
+
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin("copy").tap(opts => {
+                opts[0][0].ignore.push({glob: "index-development.html"});
+                return opts;
+            });
+        }
+
+    }
 }
