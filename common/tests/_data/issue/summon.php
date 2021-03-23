cd3d0e90c4d8d11ef680ff1a2fe0fa75add06839
arturpezda <atipezda@gmail.com>
2021-03-23 08:39:30 +0100

    FIX: missing properties in summon fixture FEAT: new summon fixtures for calendar testing

--- common/tests/_data/issue/summon.php
+++ common/tests/_data/issue/summon.php
@@ -4,6 +4,11 @@ use common\fixtures\helpers\TerytFixtureHelper;
 use common\fixtures\helpers\UserFixtureHelper;
 use common\models\issue\Summon;
 
+$todayDate = date('Y-m-d');
+$todayEventSevenAmDate = $todayDate . " " . "7:00:00";
+$todayEventTenAmDate = $todayDate . " " . "10:00:00";
+$todayEventElevenAmDate = $todayDate . " " . "11:00:00";
+
 return [
 	'new' => [
 		'status' => Summon::STATUS_NEW,
@@ -14,6 +19,9 @@ return [
 		'contractor_id' => 300,
 		'city_id' => TerytFixtureHelper::SIMC_ID_BIELSKO_BIALA,
 		'entity_id' => 1,
+		'created_at' => '1600757862',
+		'updated_at' => '1600757862',
+		'start_at' => '2020-09-22 00:00:00'
 	],
 	'in-progress' => [
 		'status' => Summon::STATUS_IN_PROGRESS,
@@ -24,25 +32,61 @@ return [
 		'contractor_id' => UserFixtureHelper::CUSTOMER_JOHN_WAYNE_ID,
 		'city_id' => TerytFixtureHelper::SIMC_ID_DUCHOWO,
 		'entity_id' => 2,
+		'created_at' => '1600757862',
+		'updated_at' => '1600757862',
+		'start_at' => '2020-09-22 00:00:00',
 	],
 	'in-progress-301' => [
 		'status' => Summon::STATUS_IN_PROGRESS,
 		'type' => Summon::TYPE_APPEAL,
-		'title' => 'In progress summon',
+		'title' => 'In progress summon 301',
 		'issue_id' => 2,
 		'owner_id' => 301,
 		'contractor_id' => UserFixtureHelper::CUSTOMER_JOHN_WAYNE_ID,
 		'city_id' => TerytFixtureHelper::SIMC_ID_BIELSKO_BIALA,
 		'entity_id' => 1,
+		'created_at' => '1600757862',
+		'updated_at' => '1600757862',
+		'start_at' => '2020-09-22 00:00:00',
 	],
 	'realized' => [
 		'status' => Summon::STATUS_REALIZED,
 		'type' => Summon::TYPE_ANTIVINDICATION,
-		'title' => 'In progress summon',
+		'title' => 'realized summon',
 		'issue_id' => 3,
 		'owner_id' => 301,
 		'contractor_id' => 300,
 		'city_id' => TerytFixtureHelper::SIMC_ID_BIELSKO_BIALA,
 		'entity_id' => 1,
+		'created_at' => '1600757862',
+		'updated_at' => '1600757862',
+		'start_at' => '2020-09-22 00:00:00',
 	],
+	'beautiful-day-summon' => [
+		'status' => Summon::STATUS_IN_PROGRESS,
+		'type' => Summon::TYPE_APPEAL,
+		'title' => 'some summon',
+		'issue_id' => 1,
+		'owner_id' => 300,
+		'contractor_id' => 404,
+		'city_id' => TerytFixtureHelper::SIMC_ID_DUCHOWO,
+		'entity_id' => 2,
+		'created_at' => '1600757862',
+		'updated_at' => '1600757862',
+		'start_at' => $todayEventElevenAmDate,
+	],
+	'pretty-day-summon' =>[
+		'status' => Summon::STATUS_NEW,
+		'type' => Summon::TYPE_APPEAL,
+		'title' => 'Document summon',
+		'issue_id' => 1,
+		'owner_id' => 300,
+		'contractor_id' => 404,
+		'city_id' => TerytFixtureHelper::SIMC_ID_BIELSKO_BIALA,
+		'entity_id' => 1,
+		'created_at' => '1600757862',
+		'updated_at' => '1600757862',
+		'start_at' => $todayEventSevenAmDate,
+		'deadline_at' => $todayEventTenAmDate
+	]
 ];
