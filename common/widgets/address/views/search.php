b92a427b56be16a1dca8e4c9fd4761ce766d48db
edzima <fachowosc@gmail.com>
2021-05-06 07:39:57 +0200

    feat: add district filter in AddressSearch. feat: add commune filter in AddressSearch. feat: add DepDrop fields for district and commune in address search form.

--- common/widgets/address/views/search.php
+++ common/widgets/address/views/search.php
@@ -1,6 +1,9 @@
 <?php
 
+use common\helpers\Html;
+use common\helpers\Url;
 use common\models\AddressSearch;
+use kartik\depdrop\DepDrop;
 use kartik\select2\Select2;
 use yii\web\View;
 use yii\widgets\ActiveForm;
@@ -11,18 +14,70 @@ use yii\widgets\ActiveForm;
 ?>
 <div class="address-search-widget">
 	<div class="row">
-		<?=
-		$form->field($model, 'region_id', [
-			'options' => [
-				'class' => 'col-md-3',
-			],
-		])->widget(Select2::class, [
-			'data' => $model->getRegionsNames(),
-			'options' => [
-				'placeholder' => $model->getAttributeLabel('region_id'),
-			],
+
+
+		<?= $form->field($model, 'region_id', [
+			'options' => ['class' => 'col-md-4'],
 		])
+			->widget(Select2::class, [
+					'data' => $model->getRegionsData(),
+					'options' => [
+						'id' => Html::getInputId($model, 'region_id'),
+						'placeholder' => $model->getAttributeLabel('region_id'),
+					],
+				]
+			)
 		?>
+
+		<?= $form->field($model, 'district_id', [
+			'options' => ['class' => 'col-md-4'],
+		])->widget(DepDrop::class, [
+			'type' => DepDrop::TYPE_SELECT2,
+			'options' => [
+				'id' => Html::getInputId($model, 'district_id'),
+				'placeholder' => Yii::t('common', 'Select...'),
+			],
+			'data' => $model->getDistrictsData(),
+			'pluginOptions' => [
+				'depends' => [Html::getInputId($model, 'region_id')],
+				'url' => Url::to(['/teryt/terc/district-list']),
+				'loading' => Yii::t('common', 'Loading...'),
+				'params' => [Html::getInputId($model, 'district_id')],
+			],
+			'select2Options' => [
+				'pluginOptions' => [
+					'allowClear' => true,
+				],
+			],
+		]); ?>
+
+		<?= $form->field($model, 'commune_id', [
+			'options' => ['class' => 'col-md-4'],
+		])
+			->widget(DepDrop::class, [
+				'type' => DepDrop::TYPE_SELECT2,
+				'data' => $model->getCommunesData(),
+				'options' => [
+					Html::getInputId($model, 'commune_id'),
+					'placeholder' => Yii::t('common', 'Select...'),
+				],
+				'pluginOptions' => [
+					'depends' => [
+						Html::getInputId($model, 'region_id'),
+						Html::getInputId($model, 'district_id'),
+					],
+					'url' => Url::to(['/teryt/terc/commune-list']),
+					'loading' => Yii::t('common', 'Loading...'),
+				],
+				'select2Options' => [
+					'pluginOptions' => [
+						'allowClear' => true,
+					],
+				],
+			])
+		?>
+
+
 
 		<?= $form->field($model, 'postal_code', [
 			'options' => [
