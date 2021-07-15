a640f7b0a8518bf6f22d0277ac3fc13273eead28
edzima <fachowosc@gmail.com>
2021-07-15 11:58:33 +0200

    feat: LandingLeadForm provider as Form.

--- common/modules/lead/models/forms/LandingLeadForm.php
+++ common/modules/lead/models/forms/LandingLeadForm.php
@@ -2,11 +2,13 @@
 
 namespace common\modules\lead\models\forms;
 
+use common\modules\lead\models\Lead;
 use common\modules\lead\models\LeadStatusInterface;
 
 class LandingLeadForm extends LeadForm {
 
 	public $status_id = LeadStatusInterface::STATUS_NEW;
+	public $provider = Lead::PROVIDER_FORM;
 
 	public function load($data, $formName = ''): bool {
 		return parent::load($data, $formName);
