54b2ed270261658908749494d62882bdbd64fd9e
edzima <fachowosc@gmail.com>
2021-08-13 09:08:35 +0200

    feat: add provision user link in ProvisionUserGrid.

--- common/widgets/grid/ProvisionUserGrid.php
+++ common/widgets/grid/ProvisionUserGrid.php
@@ -2,13 +2,21 @@
 
 namespace common\widgets\grid;
 
+use common\helpers\Html;
+use common\models\provision\Provision;
+use common\models\user\User;
 use common\widgets\GridView;
+use Yii;
 
 class ProvisionUserGrid extends GridView {
 
 	public $summary  = '';
 
 	public function init(): void {
+		if ($this->withLink === null) {
+			$this->withLink = Yii::$app->user->can(User::PERMISSION_PROVISION);
+		}
+
 		if (empty($this->columns)) {
 			$this->columns = $this->defaultColumns();
 		}
@@ -18,8 +26,35 @@ class ProvisionUserGrid extends GridView {
 	public function defaultColumns(): array {
 		return [
 			'type.name',
-			'toUser',//@todo add link to provision user schema for User::PERMISSION_PROVISION
-			'fromUserString',//@todo add link to provision user schema for User::PERMISSION_PROVISION
+			[
+				'attribute' => 'toUser',
+				'format' => 'raw',
+				'value' => function (Provision $provision): string {
+					$value = Html::encode($provision->toUser->getFullName());
+					if (!$this->withLink) {
+						return $value;
+					}
+
+					return Html::a($value,
+						['/provision/user/user-view', 'userId' => $provision->to_user_id, 'typeId' => $provision->type_id], [
+							'target' => '_blank',
+						]);
+				},
+			],
+			[
+				'attribute' => 'fromUserString',
+				'format' => 'raw',
+				'value' => function (Provision $provision): string {
+					$value = Html::encode($provision->getFromUserString());
+					if (!$this->withLink) {
+						return $value;
+					}
+					return Html::a($value,
+						['/provision/user/user-view', 'userId' => $provision->from_user_id, 'typeId' => $provision->type_id], [
+							'target' => '_blank',
+						]);
+				},
+			],
 			'provision',
 			'value:currency',
 		];
