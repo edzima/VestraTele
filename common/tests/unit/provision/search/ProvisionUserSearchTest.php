83c34b5f1c591fc4baf9fc1c2afc19276d50876c
edzima <fachowosc@gmail.com>
2021-08-16 11:06:24 +0200

    test: refactor ProvisionUserSearchTest (remove assertTotalCount())

--- common/tests/unit/provision/search/ProvisionUserSearchTest.php
+++ common/tests/unit/provision/search/ProvisionUserSearchTest.php
@@ -4,15 +4,19 @@ namespace common\tests\unit\provision\search;
 
 use common\fixtures\helpers\ProvisionFixtureHelper;
 use common\fixtures\helpers\UserFixtureHelper;
+use common\models\provision\ProvisionUser;
 use common\models\provision\ProvisionUserSearch;
 use common\tests\_support\UnitSearchModelTrait;
 use common\tests\unit\Unit;
 
+/**
+ * @property ProvisionUserSearch $model
+ */
 class ProvisionUserSearchTest extends Unit {
 
 	use UnitSearchModelTrait;
 
-	public function _before() {
+	public function _before(): void {
 		$this->model = $this->createModel();
 		$this->tester->haveFixtures(array_merge(
 			ProvisionFixtureHelper::user(),
@@ -21,31 +25,76 @@ class ProvisionUserSearchTest extends Unit {
 		parent::_before();
 	}
 
-	public function testEmpty(): void {
-		$this->assertTotalCount(3);
-	}
-
 	public function testOnlySelf(): void {
-		$this->assertTotalCount(2, ['onlySelf' => true]);
+		$models = $this->search(['onlySelf' => true])->getModels();
+		$this->tester->assertNotEmpty($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertTrue($model->isSelf());
+		}
 	}
 
 	public function testFromUser(): void {
-		$this->assertTotalCount(2, ['from_user_id' => UserFixtureHelper::AGENT_PETER_NOWAK]);
-		$this->assertTotalCount(1, ['from_user_id' => UserFixtureHelper::AGENT_AGNES_MILLER]);
+		$models = $this->search(['from_user_id' => UserFixtureHelper::AGENT_PETER_NOWAK])->getModels();
+		$this->tester->assertNotEmpty($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertSame($model->from_user_id, UserFixtureHelper::AGENT_PETER_NOWAK);
+		}
 
-		$this->assertTotalCount(1, ['from_user_id' => UserFixtureHelper::AGENT_PETER_NOWAK, 'onlySelf' => true]);
-		$this->assertTotalCount(1, ['from_user_id' => UserFixtureHelper::AGENT_AGNES_MILLER, 'onlySelf' => true]);
+		$models = $this->search(['from_user_id' => UserFixtureHelper::AGENT_AGNES_MILLER])->getModels();
+		$this->tester->assertNotEmpty($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertSame($model->from_user_id, UserFixtureHelper::AGENT_AGNES_MILLER);
+		}
+
+		$models = $this->search([
+			'from_user_id' => UserFixtureHelper::AGENT_AGNES_MILLER,
+			'onlySelf' => true,
+		])->getModels();
+		$this->tester->assertNotEmpty($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertSame($model->from_user_id, UserFixtureHelper::AGENT_AGNES_MILLER);
+			$this->tester->assertTrue($model->isSelf());
+		}
 	}
 
 	public function testToUser(): void {
-		$this->assertTotalCount(1, ['to_user_id' => UserFixtureHelper::AGENT_PETER_NOWAK]);
-		$this->assertTotalCount(2, ['to_user_id' => UserFixtureHelper::AGENT_AGNES_MILLER]);
+		$models = $this->search(['to_user_id' => UserFixtureHelper::AGENT_PETER_NOWAK])->getModels();
+		$this->tester->assertNotEmpty($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertSame($model->to_user_id, UserFixtureHelper::AGENT_PETER_NOWAK);
+		}
+
+		$models = $this->search(['to_user_id' => UserFixtureHelper::AGENT_AGNES_MILLER])->getModels();
+		$this->tester->assertNotEmpty($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertSame($model->to_user_id, UserFixtureHelper::AGENT_AGNES_MILLER);
+		}
 	}
 
 	public function testOverwritten(): void {
-		$this->assertTotalCount(3, ['overwritten' => null]);
-		$this->assertTotalCount(2, ['overwritten' => true]);
-		$this->assertTotalCount(1, ['overwritten' => false]);
+		$models = $this->search(['overwritten' => true])->getModels();
+		$this->tester->assertNotEmpty($models);
+		$overwrittenCount = count($models);
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertTrue($model->getIsOverwritten());
+		}
+
+		$models = $this->search(['overwritten' => false])->getModels();
+		$this->tester->assertNotEmpty($models);
+		$dontOverwrittenCount = count($models);
+
+		foreach ($models as $model) {
+			/** @var ProvisionUser $model */
+			$this->tester->assertFalse($model->getIsOverwritten());
+		}
+		$this->tester->assertSame($this->search(['overwritten' => null])->getTotalCount(), $overwrittenCount + $dontOverwrittenCount);
 	}
 
 	protected function createModel(): ProvisionUserSearch {
