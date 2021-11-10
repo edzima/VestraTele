bf41151c998e52d4f2b4815560d65a1a9e2efc28
edzima <fachowosc@gmail.com>
2021-11-10 12:32:32 +0100

    fix: Log search message rule as safe.

--- backend/models/search/LogSearch.php
+++ backend/models/search/LogSearch.php
@@ -9,61 +9,58 @@ use backend\models\Log;
 /**
  * LogSearch represents the model behind the search form about `backend\models\Log`.
  */
-class LogSearch extends Log
-{
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['id', 'log_time', 'message'], 'integer'],
-            [['category', 'prefix', 'level'], 'safe'],
-        ];
-    }
+class LogSearch extends Log {
 
-    /**
-     * @inheritdoc
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules() {
+		return [
+			[['id', 'log_time'], 'integer'],
+			[['category', 'prefix', 'level', 'message'], 'safe'],
+		];
+	}
 
-    /**
-     * Creates data provider instance with search query applied.
-     *
-     * @param array $params
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query = Log::find();
+	/**
+	 * @inheritdoc
+	 */
+	public function scenarios() {
+		// bypass scenarios() implementation in the parent class
+		return Model::scenarios();
+	}
 
-        // add conditions that should always apply here
+	/**
+	 * Creates data provider instance with search query applied.
+	 *
+	 * @param array $params
+	 * @return ActiveDataProvider
+	 */
+	public function search($params) {
+		$query = Log::find();
 
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-            'pagination' => [
-                'pagesize' => 60,
-            ],
-        ]);
+		// add conditions that should always apply here
 
-        if (!($this->load($params) && $this->validate())) {
-            return $dataProvider;
-        }
+		$dataProvider = new ActiveDataProvider([
+			'query' => $query,
+			'pagination' => [
+				'pagesize' => 60,
+			],
+		]);
 
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'level' => $this->level,
-            'log_time' => $this->log_time,
-            'message' => $this->message,
-        ]);
+		if (!($this->load($params) && $this->validate())) {
+			return $dataProvider;
+		}
 
-        $query->andFilterWhere(['like', 'category', $this->category])
-            ->andFilterWhere(['like', 'prefix', $this->prefix]);
+		$query->andFilterWhere([
+			'id' => $this->id,
+			'level' => $this->level,
+			'log_time' => $this->log_time,
+			'message' => $this->message,
+		]);
 
-        return $dataProvider;
-    }
+		$query->andFilterWhere(['like', 'category', $this->category])
+			->andFilterWhere(['like', 'prefix', $this->prefix]);
+
+		return $dataProvider;
+	}
 }
