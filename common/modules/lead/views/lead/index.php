a959714ca52119f487212c9dea7634d12a3e42e1
edzima <fachowosc@gmail.com>
2021-12-09 15:19:22 +0100

    Revert "feat: add hide/visible for multiple action for Lead index page."
    This reverts commit 61a5f6128d3fe69791176e23f6a88fe37e6c1a41.

--- common/modules/lead/views/lead/index.php
+++ common/modules/lead/views/lead/index.php
@@ -40,7 +40,6 @@ foreach (LeadSearch::questions() as $question) {
 
 $js = <<<JS
 const multipleForm = document.getElementById('multiple-form-wrap');
-if(multipleForm){
 const leadsGrid = jQuery("#leads-grid");
 leadsGrid.find("input[type='checkbox']").on('click',function (){
 	setTimeout(function (){
@@ -51,9 +50,7 @@ leadsGrid.find("input[type='checkbox']").on('click',function (){
 			multipleForm.classList.add('hidden');
 		}
 	}, 100);
-});	
-}
-
+});
 JS;
 
 $this->registerJs($js);
