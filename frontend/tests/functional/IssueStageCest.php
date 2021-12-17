3829ddc8d32c22de7c5da56b4c434ab95d2be6ad
edzima <fachowosc@gmail.com>
2021-12-17 11:57:33 +0100

    test: IssueStageCest success submit form.

--- frontend/tests/functional/IssueStageCest.php
+++ frontend/tests/functional/IssueStageCest.php
@@ -3,6 +3,8 @@
 namespace frontend\tests\functional;
 
 use common\fixtures\helpers\IssueFixtureHelper;
+use common\models\issue\Issue;
+use common\models\issue\IssueNote;
 use common\models\user\Worker;
 use frontend\controllers\IssueController;
 use frontend\tests\_support\CustomerServiceTester;
@@ -34,7 +36,27 @@ class IssueStageCest {
 		$I->see('Change Stage: ');
 	}
 
-	public function sendEmpty(): void {
+	public function tryChange(CustomerServiceTester $I): void {
+		$I->amLoggedIn();
+		$I->assignPermission(Worker::PERMISSION_ISSUE_STAGE_CHANGE);
+		/** @var Issue $issue */
+		$issue = $I->grabFixture(IssueFixtureHelper::ISSUE, 0);
+		$I->amOnPage([static::ROUTE, 'issueId' => $issue->getIssueId()]);
+		$I->submitForm('#issue-stage-form', [
+			'IssueStageChangeForm[stage_id]' => 2,
+		]);
 
+		$I->seeInCurrentUrl(IssueViewCest::ROUTE_VIEW);
+		$I->seeRecord(Issue::class, [
+			'id' => $issue->getIssueId(),
+			'stage_id' => 2,
+		]);
+		$I->seeRecord(IssueNote::class, [
+			'issue_id' => $issue->getIssueId(),
+			'type' => IssueNote::generateType(
+				IssueNote::generateType(IssueNote::TYPE_STAGE_CHANGE, 2),
+				1
+			),
+		]);
 	}
 }
