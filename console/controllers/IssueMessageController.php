630fba993efba302ebec5515869d6577bd1dac5f
edzima <fachowosc@gmail.com>
2022-01-04 11:19:55 +0100

    feat: add issue agent sms to customer from console.

---
+++ console/controllers/IssueMessageController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace console\controllers;
+
+use common\models\issue\IssueUser;
+use common\models\issue\query\IssueQuery;
+use common\models\KeyStorageItem;
+use common\models\message\IssueSmsForm;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class IssueMessageController extends Controller {
+
+	public function actionAgentToCustomer(int $agentId, string $message = null, bool $withArchive = false): void {
+		$models = IssueUser::find()
+			->andWhere([IssueUser::tableName() . '.user_id' => $agentId])
+			->withType(IssueUser::TYPE_AGENT)
+			->joinWith([
+				'issue' => function (IssueQuery $query) use ($withArchive) {
+					if (!$withArchive) {
+						$query->withoutArchives();
+					}
+				},
+			])
+			->joinWith('issue.customer.userProfile')
+			->all();
+		$push = 0;
+		foreach ($models as $model) {
+			$issue = $model->issue;
+			$messageForm = new IssueSmsForm($issue, ['userTypes' => [IssueUser::TYPE_CUSTOMER]]);
+			$messageForm->owner_id = $this->getSmsOwnerId();
+			$messageForm->message = $message;
+			$messageForm->setFirstAvailablePhone();
+			if ($messageForm->pushJob()) {
+				$push++;
+			} else {
+				Console::output(print_r($messageForm->getErrors()));
+			}
+		}
+		Console::output('All Models: ' . count($models));
+		Console::output('Push SMS: ' . $push);
+	}
+
+	private function getSmsOwnerId(): int {
+		$owner = Yii::$app->keyStorage->get(KeyStorageItem::KEY_ROBOT_SMS_OWNER_ID);
+		if ($owner === null) {
+			throw new InvalidConfigException('Not Set Robot SMS Owner. Key: (' . KeyStorageItem::KEY_ROBOT_SMS_OWNER_ID . ').');
+		}
+		return $owner;
+	}
+
+}
