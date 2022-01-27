515dbdb7cc16b0ae0ec6b7be435a7b5f4e4b7a41
edzima <fachowosc@gmail.com>
2022-01-27 15:20:58 +0100

    feat: create TooltipAsset with Tippy.js

---
+++ common/assets/TooltipAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace common\assets;
+
+use yii\helpers\Json;
+use yii\web\AssetBundle;
+
+class TooltipAsset extends AssetBundle {
+
+	public const DEFAULT_ATTRIBUTE_NAME = 'data-tippy-content';
+
+	public $baseUrl = 'https://unpkg.com/';
+
+	public $css = ['tippy.js@5/dist/tippy.css'];
+	public $js = [
+		'popper.js@1',
+		YII_ENV_DEV
+			? 'tippy.js@5/dist/tippy-bundle.iife.js'
+			: 'tippy.js@5',
+	];
+
+	public static function initScript(?string $selector = null, array $options = []): string {
+		if ($selector === null) {
+			$selector = '[' . static::DEFAULT_ATTRIBUTE_NAME . ']';
+		}
+		if (!empty($options)) {
+			$json = Json::encode($options);
+			return "tippy('$selector', $json);";
+		}
+		return "tippy('$selector');";
+	}
+
+}
