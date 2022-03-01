fbbe7cba607cb925f8caa9d5e872ba33dec8c9b9
edzima <fachowosc@gmail.com>
2022-03-01 12:25:38 +0100

    fix: redirect to LeadDialer view page after single create.

--- common/modules/lead/controllers/DialerController.php
+++ common/modules/lead/controllers/DialerController.php
@@ -68,7 +68,7 @@ class DialerController extends BaseController {
 		$model->leadId = $id;
 
 		if ($model->load(Yii::$app->request->post()) && $model->save()) {
-			return $this->redirect(['view', 'id' => $model->id]);
+			return $this->redirect(['view', 'id' => $model->getModel()->id]);
 		}
 
 		return $this->render('create', [
