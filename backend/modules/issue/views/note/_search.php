a445d3231efed209c6dca14467a8d76fddc38eee
edzima <fachowosc@gmail.com>
2022-06-27 11:29:02 +0200

    feat: reset button as index link in issue note search.

--- backend/modules/issue/views/note/_search.php
+++ backend/modules/issue/views/note/_search.php
@@ -29,7 +29,10 @@ use yii\widgets\ActiveForm;
 
 	<div class="form-group">
 		<?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-		<?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+		<?= Html::a(Yii::t('backend', 'Reset'),
+			['index'], [
+				'class' => 'btn btn-default',
+			]) ?>
 	</div>
 
 	<?php ActiveForm::end(); ?>
