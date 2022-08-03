e33383a5bb84166b0523db23a72de84976436274
edzima <fachowosc@gmail.com>
2022-08-03 13:08:50 +0200

    chown: Lead Active Record as Abstract Class.

--- common/modules/lead/models/ActiveRecord.php
+++ common/modules/lead/models/ActiveRecord.php
@@ -7,7 +7,7 @@ use Yii;
 use yii\db\ActiveRecord as BaseActiveRecord;
 use yii\db\Connection;
 
-class ActiveRecord extends BaseActiveRecord {
+abstract class ActiveRecord extends BaseActiveRecord {
 
 	public static function getDb(): Connection {
 		return Module::getInstance()->db ?: Yii::$app->getDb();
