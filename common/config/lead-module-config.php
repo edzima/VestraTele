1e936f73699c17527936dfbd4d80cbda91ab4661
edzima <fachowosc@gmail.com>
2022-08-08 10:00:58 +0200

    test: config for lead module database.

--- common/config/lead-module-config.php
+++ common/config/lead-module-config.php
@@ -1,6 +1,8 @@
 <?php
 
+use common\helpers\ArrayHelper;
 use common\models\user\User;
+use common\modules\lead\components\LeadDialerManager;
 use common\modules\lead\Module;
 use yii\db\Connection;
 
@@ -9,9 +11,21 @@ $config = [
 	'userClass' => User::class,
 ];
 
-if (isset($_ENV['DB_LEAD_DSN']) &&
-	$_ENV['DB_LEAD_DSN'] !== $_ENV['DB_DSN']
-) {
+if (YII_ENV_TEST) {
+	$config['userNames'] = static function () {
+		return ArrayHelper::map(User::find()->asArray()->all(), 'id', 'username');
+	};
+	$config['dialer'] = [
+		'class' => LeadDialerManager::class,
+		'callingStatus' => 2,
+		'notAnsweredStatus' => 3,
+		'answeredStatus' => 4,
+	];
+}
+
+if (!YII_ENV_TEST
+	&& isset($_ENV['DB_LEAD_DSN']) &&
+	$_ENV['DB_LEAD_DSN'] !== $_ENV['DB_DSN']) {
 	$config['components']['db'] = [
 		'class' => Connection::class,
 		'dsn' => getenv('DB_LEAD_DSN'),
