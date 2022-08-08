6d793fa12ba4cbeb3bbff011351dc7415148778d
edzima <fachowosc@gmail.com>
2022-08-08 10:00:58 +0200

    test: config for lead module database.

--- common/config/test.php
+++ common/config/test.php
@@ -1,9 +1,6 @@
 <?php
 
 use common\models\user\User;
-use common\modules\lead\components\LeadDialerManager;
-use common\modules\lead\Module;
-use yii\helpers\ArrayHelper;
 
 return [
 	'id' => 'app-common-tests',
@@ -17,21 +14,6 @@ return [
 		],
 		'authManager' => [
 			'cache' => null,
-		],
-	],
-	'modules' => [
-		'lead' => [
-			'class' => Module::class,
-			'userClass' => User::class,
-			'userNames' => static function () {
-				return ArrayHelper::map(User::find()->asArray()->all(), 'id', 'username');
-			},
-			'dialer' => [
-				'class' => LeadDialerManager::class,
-				'callingStatus' => 2,
-				'notAnsweredStatus' => 3,
-				'answeredStatus' => 4,
-			],
 		],
 	],
 ];
