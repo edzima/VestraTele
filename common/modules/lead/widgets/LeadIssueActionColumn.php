7fbe19cd5ea435e94a93268c82a9aa36cdcb6ae1
edzima <fachowosc@gmail.com>
2022-08-22 10:43:43 +0200

    feat: add returnUrl to LeadIssueActionColumn.

--- common/modules/lead/widgets/LeadIssueActionColumn.php
+++ common/modules/lead/widgets/LeadIssueActionColumn.php
@@ -2,15 +2,18 @@
 
 namespace common\modules\lead\widgets;
 
+use common\modules\lead\controllers\IssueController;
 use common\modules\lead\models\LeadIssue;
 use common\widgets\grid\ActionColumn;
 
 class LeadIssueActionColumn extends ActionColumn {
 
+	/** @see IssueController */
 	public $controller = '/lead/issue';
 	public $template = '{confirm} {unconfirm}';
+	public ?string $returnUrl = null;
 
-	public function init() {
+	public function init(): void {
 		parent::init();
 		if (empty($this->visibleButtons)) {
 			$this->visibleButtons = [
@@ -34,5 +37,14 @@ class LeadIssueActionColumn extends ActionColumn {
 		$this->initDefaultButton('unconfirm', 'unchecked', [
 			'data-method' => 'post',
 		]);
+	}
+
+	public function createUrl($action, $model, $key, $index) {
+		$url = parent::createUrl($action, $model, $key, $index);
+		$actions = ['confirm', 'unconfirm'];
+		if ($this->returnUrl && in_array($action, $actions, true)) {
+			$url .= '&returnUrl=' . $this->returnUrl;
+		}
+		return $url;
 	}
 }
