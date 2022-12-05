011287139fe9f20f2d04d7d3fdaca2532bb940fc
edzima <fachowosc@gmail.com>
2022-12-05 12:52:57 +0100

    feat: add tags search field to frontend index page.

--- frontend/views/issue/_search.php
+++ frontend/views/issue/_search.php
@@ -4,6 +4,7 @@ use common\widgets\ActiveForm;
 use common\widgets\DateWidget;
 use frontend\helpers\Html;
 use frontend\models\search\IssueSearch;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model IssueSearch */
@@ -19,10 +20,25 @@ use frontend\models\search\IssueSearch;
 	<div class="row">
 
 		<?= $form->field($model, 'createdAtFrom', ['options' => ['class' => 'col-md-3 col-lg-2']])
-			->widget(DateWidget::class) ?>
+			->widget(DateWidget::class)
+		?>
 
 		<?= $form->field($model, 'createdAtTo', ['options' => ['class' => 'col-md-3 col-lg-2']])
-			->widget(DateWidget::class) ?>
+			->widget(DateWidget::class)
+		?>
+
+		<?= $form->field($model, 'tagsIds', ['options' => ['class' => 'col-md-8 col-lg-6']])->widget(Select2::class, [
+			'data' => IssueSearch::getTagsNames(),
+			'options' => [
+				'multiple' => true,
+				'placeholder' => Yii::t('issue', 'Tags'),
+			],
+			'pluginOptions' => [
+				'allowClear' => true,
+			],
+			'showToggleAll' => false,
+		]) ?>
+
 
 	</div>
 
