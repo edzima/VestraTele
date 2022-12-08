34332eaca6a184dd9e2b60eb6b582d61b8758159
edzima <fachowosc@gmail.com>
2022-12-08 11:29:41 +0100

    feat: remove LegacyAddress from Address DetailView.

--- common/widgets/address/AddressDetailView.php
+++ common/widgets/address/AddressDetailView.php
@@ -3,7 +3,6 @@
 namespace common\widgets\address;
 
 use common\models\Address;
-use common\models\address\Address as LegacyAddress;
 use Yii;
 use yii\widgets\DetailView;
 
@@ -14,56 +13,50 @@ use yii\widgets\DetailView;
  */
 class AddressDetailView extends DetailView {
 
-	/** @var Address | LegacyAddress */
+	/** @var Address */
 	public $model;
 
 	public function init(): void {
 		if (empty($this->attributes)) {
-			if ($this->model instanceof LegacyAddress) {
-				$this->attributes = [
-					'cityId',
-					'state',
-					'province',
-					'subProvince',
-					[
-						'attribute' => 'city',
-						'label' => 'Miasto',
-						'value' => Yii::$app->formatter->asCityCode($this->model->getCity(), $this->model->cityCode),
-					],
-					'street',
-				];
-			} else {
-				$this->attributes = [
-					[
-						'attribute' => 'city.terc.region',
-						'label' => Yii::t('address', 'Region'),
-						'visible' => !empty($this->model->city),
-					],
-					[
-						'attribute' => 'city.terc.district',
-						'label' => Yii::t('address', 'District'),
-						'visible' => !empty($this->model->city),
-					],
-					[
-						'attribute' => 'city.terc.commune',
-						'label' => Yii::t('address', 'Commune'),
-						'visible' => !empty($this->model->city),
-					],
-					[
-						'attribute' => 'city',
-						'label' => Yii::t('address', 'City'),
-						'value' => $this->model->city ? Yii::$app->formatter->asCityCode($this->model->city->name, $this->model->postal_code) : $this->model->postal_code,
-						'visible' => !empty($this->model->postal_code) || !empty($this->model->city),
-					],
-					[
-						'attribute' => 'info',
-						'label' => Yii::t('address', 'Info'),
-						'visible' => !empty($this->model->info),
-					],
-				];
-			}
+			$this->attributes = $this->defaultAttributes();
 		}
 		parent::init();
+	}
+
+	public function defaultAttributes(): array {
+		return [
+			[
+				'attribute' => 'city.terc.region',
+				'label' => Yii::t('address', 'Region'),
+				'visible' => !empty($this->model->city),
+			],
+			[
+				'attribute' => 'city.terc.district',
+				'label' => Yii::t('address', 'District'),
+				'visible' => !empty($this->model->city),
+			],
+			[
+				'attribute' => 'city.terc.commune',
+				'label' => Yii::t('address', 'Commune'),
+				'visible' => !empty($this->model->city),
+			],
+			[
+				'attribute' => 'city',
+				'label' => Yii::t('address', 'City'),
+				'value' => Yii::$app->formatter->asCityCode(
+					$this->model->city
+						? $this->model->city->name
+						: null,
+					$this->model->postal_code),
+				'visible' => !empty($this->model->postal_code) || !empty($this->model->city),
+				'format' => 'html',
+			],
+			[
+				'attribute' => 'info',
+				'label' => Yii::t('address', 'Info'),
+				'visible' => !empty($this->model->info),
+			],
+		];
 	}
 
 }
