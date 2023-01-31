8cab67d29e4923dcd2097b4eab02d1e9c7ab160d
edzima <fachowosc@gmail.com>
2023-01-31 12:02:45 +0100

    feat: add issue note email permissions. BREAKING_CHANGE: rbac/create-not-exist.

--- common/models/user/Worker.php
+++ common/models/user/Worker.php
@@ -38,6 +38,10 @@ class Worker extends User implements ActiveHierarchy {
 	public const PERMISSION_ISSUE_TAG_MANAGER = 'issue.tag.manager';
 	public const PERMISSION_ISSUE_TYPE_MANAGER = 'issue.type.manager';
 
+	public const PERMISSION_ISSUE_NOTE_EMAIL_MESSAGE_ISSUE = 'issue.message.note.issue';
+	public const PERMISSION_ISSUE_NOTE_EMAIL_MESSAGE_SUMMON = 'issue.message.note.summon';
+	public const PERMISSION_ISSUE_NOTE_EMAIL_MESSAGE_SETTLEMENT = 'issue.message.note.settlement';
+
 	public const PERMISSION_NOTE_TEMPLATE = 'note.template';
 	public const PERMISSION_COST_DEBT = 'cost.debt';
 	public const PERMISSION_PROVISION_CHILDREN_VISIBLE = 'provision.children.visible';
