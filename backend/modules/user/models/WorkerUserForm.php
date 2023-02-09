fa19bedb2c44cee4684ab56588893630887be502
edzima <fachowosc@gmail.com>
2023-02-09 10:21:51 +0100

    feat: allow applyAuth for not new record in WorkerUserForm.

--- backend/modules/user/models/WorkerUserForm.php
+++ backend/modules/user/models/WorkerUserForm.php
@@ -2,7 +2,6 @@
 
 namespace backend\modules\user\models;
 
-use common\models\user\Customer;
 use common\models\user\User;
 use common\models\user\Worker;
 
@@ -11,10 +10,6 @@ class WorkerUserForm extends UserForm {
 	protected const EXCLUDED_ROLES = [
 		User::ROLE_ADMINISTRATOR,
 		User::ROLE_BOOKKEEPER,
-		Customer::ROLE_CUSTOMER,
-		Customer::ROLE_HANDICAPPED,
-		Customer::ROLE_SHAREHOLDER,
-		Customer::ROLE_VICTIM,
 	];
 
 	protected const EXCLUDED_PERMISSIONS = [
@@ -35,10 +30,5 @@ class WorkerUserForm extends UserForm {
 		return new Worker();
 	}
 
-	protected function applyAuth(int $id, bool $isNewRecord): void {
-		if ($isNewRecord) {
-			parent::applyAuth($id, $isNewRecord);
-		}
-	}
 
 }
