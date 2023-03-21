e5b09a1a026e970d504b4ca9a246df2da8f5d40b
edzima <fachowosc@gmail.com>
2023-03-21 13:33:48 +0100

    chown: remove header tag from create/update tags views.

--- backend/modules/issue/views/tag/update.php
+++ backend/modules/issue/views/tag/update.php
@@ -16,7 +16,6 @@ $this->params['breadcrumbs'][] = Yii::t('issue', 'Update');
 ?>
 <div class="issue-tag-update">
 
-	<h1><?= Html::encode($this->title) ?></h1>
 
 	<?= $this->render('_form', [
 		'model' => $model,
