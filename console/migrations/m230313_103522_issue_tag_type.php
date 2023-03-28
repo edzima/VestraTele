4ff4a72ba89976facfe1ebfbb38eeef4ba9d808a
edzima <fachowosc@gmail.com>
2023-03-28 14:45:32 +0200

    chown: merge migration to one file.

--- console/migrations/m230313_103522_issue_tag_type.php
+++ console/migrations/m230313_103522_issue_tag_type.php
@@ -48,12 +48,18 @@ class m230313_103522_issue_tag_type extends Migration {
 		$this->alterColumn('{{%issue_tag}}', 'type', $this->integer()->null());
 		$this->addForeignKey('{{%fk_issue_tag_type}}', '{{%issue_tag}}', 'type', '{{%issue_tag_type}}', 'id', 'SET NULL', 'CASCADE');
 		$this->dropIndex('name', '{{%issue_tag}}');
+
+		$this->addColumn('{{%issue_tag_type}}', 'sort_order', $this->smallInteger());
+		$this->addColumn('{{%issue_tag_type}}', 'link_issues_grid_position', $this->string()->null());
 	}
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeDown() {
+		$this->dropColumn('{{%issue_tag_type}}', 'sort_order');
+		$this->dropColumn('{{%issue_tag_type}}', 'link_issues_grid_position');
+
 		$this->alterColumn('{{%issue_tag}}', 'name', $this->string()->notNull()->unique());
 		$this->dropForeignKey('{{%fk_issue_tag_type}}', '{{%issue_tag}}');
 		$this->alterColumn('{{%issue_tag}}', 'type', $this->string()->null());
