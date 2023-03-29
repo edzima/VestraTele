0f190088c47b4005cbd9b80d32a68a2c926c1504
edzima <fachowosc@gmail.com>
2023-03-29 11:54:49 +0200

    feat: use bcc instead from list in market create summary emails.

--- common/modules/lead/models/forms/LeadMarketCreateSummaryEmail.php
+++ common/modules/lead/models/forms/LeadMarketCreateSummaryEmail.php
@@ -74,7 +74,7 @@ class LeadMarketCreateSummaryEmail extends Model {
 				]
 			)
 			->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->name . ' robot'])
-			->setTo($this->emails)
+			->setBcc($this->emails)
 			->setSubject(Yii::t('lead', 'New {count} Leads on Market.', [
 				'count' => $count,
 			]))
