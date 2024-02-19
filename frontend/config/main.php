43a6f68f791312a491ae7370fa2985a0ed493821
edzima <fachowosc@gmail.com>
2024-02-19 12:17:00 +0100

    feat: allow all lead users on frontend app to phone-blacklist.

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -56,6 +56,7 @@ return [
 							'lead/reminder',
 							'lead/report',
 							'lead/sms',
+							'lead/phone-blacklist',
 						],
 						'matchCallback' => static function ($rule, Action $action): bool {
 							if ($action->controller->id === 'sms') {
