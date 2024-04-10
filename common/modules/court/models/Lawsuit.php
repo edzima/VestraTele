4a4df12a4cfc9cc87ca0ab30bb255698d0d4b4e2
edzima <fachowosc@gmail.com>
2024-04-10 10:24:25 +0200

    feat: add lawsuit table & model.

---
+++ common/modules/court/models/Lawsuit.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace common\modules\court\models;
+
+use common\helpers\ArrayHelper;
+use common\models\issue\Issue;
+use common\models\user\User;
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "lawsuit".
+ *
+ * @property int $id
+ * @property int $court_id
+ * @property string|null $signature_act
+ * @property string|null $room
+ * @property string|null $details
+ * @property string|null $due_at
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string|null $location
+ * @property int $creator_id
+ * @property int|null $online
+ *
+ * @property Court $court
+ * @property User $creator
+ * @property Issue[] $issues
+ */
+class Lawsuit extends ActiveRecord {
+
+	public const LOCATION_STATIONARY = 'S';
+	public const LOCATION_ONLINE = 'O';
+
+	public const VIA_TABLE_ISSUE = '{{%lawsuit_issue}}';
+
+	public function behaviors(): array {
+		return array_merge(parent::behaviors(), [
+			[
+				'class' => TimestampBehavior::class,
+				'value' => new Expression('NOW()'),
+			],
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName(): string {
+		return '{{%lawsuit}}';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules(): array {
+		return [
+			[['court_id', 'creator_id'], 'required'],
+			[['court_id', 'creator_id'], 'integer'],
+			[['due_at', 'created_at', 'updated_at'], 'safe'],
+			[['location',], 'string', 'max' => 2],
+			[['signature_act', 'room', 'details'], 'string', 'max' => 255],
+			[['signature_act', 'room', 'details', 'location'], 'default', 'value' => null],
+			['location', 'in', 'range' => array_keys(static::getLocationNames())],
+			[['court_id'], 'exist', 'skipOnError' => true, 'targetClass' => Court::class, 'targetAttribute' => ['court_id' => 'id']],
+			[['creator_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['creator_id' => 'id']],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels(): array {
+		return [
+			'id' => Yii::t('court', 'ID'),
+			'issue_id' => Yii::t('court', 'Issue ID'),
+			'court_id' => Yii::t('court', 'Court'),
+			'courtName' => Yii::t('court', 'Court'),
+			'signature_act' => Yii::t('court', 'Signature Act'),
+			'details' => Yii::t('court', 'Details'),
+			'room' => Yii::t('court', 'Room'),
+			'due_at' => Yii::t('court', 'Due At'),
+			'created_at' => Yii::t('court', 'Created At'),
+			'updated_at' => Yii::t('court', 'Updated At'),
+			'creator_id' => Yii::t('court', 'Creator ID'),
+			'creator' => Yii::t('court', 'Creator'),
+			'location' => Yii::t('court', 'Location'),
+			'locationName' => Yii::t('court', 'Location'),
+
+		];
+	}
+
+	public function getCourtName(): string {
+		return $this->court->name;
+	}
+
+	public function getLocationName(): ?string {
+		return static::getLocationNames()[$this->location] ?? null;
+	}
+
+	/**
+	 * Gets query for [[Court]].
+	 *
+	 * @return ActiveQuery
+	 */
+	public function getCourt() {
+		return $this->hasOne(Court::class, ['id' => 'court_id']);
+	}
+
+	/**
+	 * Gets query for [[Creator]].
+	 *
+	 * @return ActiveQuery
+	 */
+	public function getCreator() {
+		return $this->hasOne(User::class, ['id' => 'creator_id']);
+	}
+
+	/**
+	 * Gets query for [[Issue]].
+	 *
+	 * @return ActiveQuery
+	 */
+	public function getIssues() {
+		return $this->hasMany(Issue::class, ['id' => 'issue_id'])
+			->viaTable(static::VIA_TABLE_ISSUE, ['lawsuit_id' => 'id']);
+	}
+
+	/**
+	 * @return int[]
+	 */
+	public function getIssuesIds(): array {
+		return ArrayHelper::getColumn($this->issues, 'id');
+	}
+
+	public function unlinkIssue(int $issueId): bool {
+		return static::getDb()
+				->createCommand()
+				->delete(static::VIA_TABLE_ISSUE, [
+						'lawsuit_id' => $this->id,
+						'issue_id' => $issueId,
+					]
+				)->execute() > 0;
+	}
+
+	public function linkIssues(array $ids): int {
+		$rows = [];
+		foreach ($ids as $id) {
+			$rows[] = [
+				'lawsuit_id' => $this->id,
+				'issue_id' => $id,
+			];
+		}
+		if (empty($rows)) {
+			return 0;
+		}
+		return static::getDb()
+			->createCommand()
+			->batchInsert(static::VIA_TABLE_ISSUE, [
+				'lawsuit_id',
+				'issue_id',
+			],
+				$rows
+			)->execute();
+	}
+
+	public static function getLocationNames(): array {
+		return [
+			static::LOCATION_STATIONARY => Yii::t('court', 'Stationary'),
+			static::LOCATION_ONLINE => Yii::t('court', 'Online'),
+		];
+	}
+
+}
