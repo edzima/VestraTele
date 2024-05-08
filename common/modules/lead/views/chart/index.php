bbfc84c40288459b67cb152d87706ce9f8ca128e
edzima <fachowosc@gmail.com>
2024-05-08 11:24:14 +0200

    feat: add chart_group column to LeadStatus.

--- common/modules/lead/views/chart/index.php
+++ common/modules/lead/views/chart/index.php
@@ -17,17 +17,65 @@ $this->title = Yii::t('lead', 'Charts');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('lead', 'Leads'), 'url' => ['lead/index']];
 $this->params['breadcrumbs'][] = $this->title;
 
-$usersData = $searchModel->getLeadsUsersCount();
-$users = User::getSelectList(array_keys($usersData));
-foreach ($usersData as $userId => $count) {
-	$name = empty($userId) ? Yii::t('lead', 'Without User') : $users[$userId];
-	$usersData['series'][] = $count;
-	$usersData['labels'][] = $name;
+$leadStatusColor = new LeadStatusColor();
+
+$leadsDaysData = $searchModel->getLeadsByDays();
+
+$testData = [];
+foreach ($leadsDaysData as $leadDaysData) {
+	$day = $leadDaysData['date'];
+	if (!isset($testData[$day])) {
+		$testData[$day] = [
+			'count' => 0,
+		];
+	}
+	$testData[$day]['data'][$leadDaysData['status_id']] = $leadDaysData['count'];
+	$testData[$day]['count'] += $leadDaysData['count'];
+}
+
+
+$leadsCountSeries = [];
+$leadDates = array_values(array_unique(ArrayHelper::getColumn($leadsDaysData, 'date')));
+$hasGroup = false;
+foreach ($leadsDaysData as $data) {
+	$statusId = $data['status_id'];
+	if (!isset($leadsCountSeries[$statusId])) {
+		$statusModel = LeadStatus::getModels()[$statusId];
+		$leadsCountSeries[$statusId] = [
+			'name' => $statusModel->name,
+			'data' => [],
+		];
+		if ($statusModel->chart_group) {
+			$leadsCountSeries[$statusId]['group'] = $statusModel->chart_group;
+			$hasGroup = true;
+		}
+		$leadsDaysData['colors'][] = $leadStatusColor->getStatusColor($statusModel);
+	}
+	//datetime
+//	$leadsCountSeries[$statusId]['data'][] = [
+//		$data['date'],
+//		(int) $data['count'],
+//	];
+	//category
+	$leadsCountSeries[$statusId]['data'][$data['date']] = (int) $data['count'];
+
+	$leadsDaysData['labels'][$data['date']] = $data['date'];
+}
+
+foreach ($leadsCountSeries as $index => $data) {
+	if ($hasGroup && !isset($data['group'])) {
+		$leadsCountSeries[$index]['group'] = Yii::t('chart', 'Other');
+	}
+	$data = $data['data'];
+	$dataWithoutStatues = [];
+	foreach ($leadDates as $date) {
+		$dataWithoutStatues[] = isset($data[$date]) ? $data[$date] : 0;
+	}
+
+	$leadsCountSeries[$index]['data'] = $dataWithoutStatues;
 }
 
 $statusData = $searchModel->getLeadStatusesCount();
-$leadStatusColor = new LeadStatusColor();
-
 foreach ($statusData as $status_id => $count) {
 	$status = LeadStatus::getModels()[$status_id];
 	$statusData['series'][] = $count;
@@ -47,96 +95,66 @@ foreach ($sourcesData as $id => $count) {
 	$source = LeadSource::getModels()[$id];
 	$sourcesData['series'][] = $count;
 	$sourcesData['labels'][] = $source->name;
-}
-$leadsDaysData = $searchModel->getLeadsByDays();
-$leadsCountSeries = [];
-$leadDates = array_values(array_unique(ArrayHelper::getColumn($leadsDaysData, 'date')));
-foreach ($leadsDaysData as $data) {
-	$statusId = $data['status_id'];
-	if (!isset($leadsCountSeries[$statusId])) {
-		$statusModel = LeadStatus::getModels()[$statusId];
-		$leadsCountSeries[$statusId] = [
-			'name' => $statusModel->name,
-			'data' => [],
-		];
-		//@todo add chart_group column to LeadStatus
-		if ($statusModel->name === 'Umowa') {
-			$leadsCountSeries[$statusId]['group'] = 'Umowa';
-		} else {
-			$leadsCountSeries[$statusId]['group'] = 'Reszta';
-		}
-		$leadsDaysData['colors'][] = $leadStatusColor->getStatusColor($statusModel);
-	}
-	//datetime
-//	$leadsCountSeries[$statusId]['data'][] = [
-//		$data['date'],
-//		(int) $data['count'],
-//	];
-	//category
-	$leadsCountSeries[$statusId]['data'][$data['date']] = (int) $data['count'];
-
-	$leadsDaysData['labels'][$data['date']] = $data['date'];
+	$sourcesData['names'][$id] = $source->name;
 }
 
-foreach ($leadsCountSeries as $index => $data) {
-	$data = $data['data'];
-	$dataWithoutStatues = [];
-	foreach ($leadDates as $date) {
-		$dataWithoutStatues[] = isset($data[$date]) ? $data[$date] : 0;
-	}
-
-	$leadsCountSeries[$index]['data'] = $dataWithoutStatues;
+$usersData = $searchModel->getLeadsUsersCount();
+$usersNames = User::getSelectList(array_keys($usersData), false);
+foreach ($usersData as $userId => $count) {
+	$name = empty($userId) ? Yii::t('lead', 'Without User') : $usersNames[$userId];
+	$usersData['series'][] = $count;
+	$usersData['labels'][] = $name;
 }
-
-//foreach ($leadsCountSeries as $index => $data) {
-//	while (count($leadsCountSeries[$index]['data']) <= 10) {
-//	//	$leadsCountSeries[$index]['data'][] = 0;
-//	}
-//}
 
 ?>
 <div class="lead-chart-index">
 	<?= $this->render('_search', [
 		'model' => $searchModel,
+		'sourcesNames' => $sourcesData['names'] ?? [],
+		'usersNames' => $usersNames,
 	]) ?>
-
 	<div class="lead-charts">
-		<div class="row">
-			<div class="col-md-12" style="height:50vh">
-				<?= ChartsWidget::widget([
-					'type' => ChartsWidget::TYPE_BAR,
-					'series' => array_values($leadsCountSeries),
-					'height' => '100%',
-					'chart' => [
-						'stacked' => true,
-						'zoom' => [
-							'enabled' => true,
-							'type' => 'x',
-							'autoScaleYaxis' => true,
+		<?php if (!empty($leadsCountSeries)): ?>
+
+			<div class="row">
+
+				<div class="col-md-12" style="height:50vh">
+					<?= ChartsWidget::widget([
+						'type' => ChartsWidget::TYPE_BAR,
+						'series' => array_values($leadsCountSeries),
+						'height' => '100%',
+						'chart' => [
+							'stacked' => true,
+							'zoom' => [
+								'enabled' => true,
+								'type' => 'x',
+								'autoScaleYaxis' => true,
+							],
 						],
-					],
-					'options' => [
-						'colors' => $leadsDaysData['colors'],
-						'plotOptions' => [
-							'bar' => [
-								'dataLabels' => [
-									'total' => [
-										'enabled' => true,
-										'position' => 'bottom',
+						'options' => [
+							'colors' => $leadsDaysData['colors'],
+							'plotOptions' => [
+								'bar' => [
+									'dataLabels' => [
+										'total' => [
+											'enabled' => true,
+											'position' => 'bottom',
+											'offsetX' => 6,
+										],
+
 									],
 								],
+
 							],
-
+							'xaxis' => [
+								'type' => 'datetime',
+								'categories' => $leadDates,
+							],
 						],
-						'xaxis' => [
-							'type' => 'datetime',
-							'categories' => $leadDates,
-						],
-					],
-				]) ?>
+					]) ?>
+				</div>
 			</div>
-		</div>
-
+		<?php endif; ?>
 		<?php
 
 		//	echo '<pre>' . \yii\helpers\VarDumper::dumpAsString($leadsCountSeries) . '</pre>';
@@ -144,123 +162,136 @@ foreach ($leadsCountSeries as $index => $data) {
 		?>
 
 		<div class="row">
-			<div class="col-sm-12 col-md-4" style="height:50vh">
-				<?= !empty($statusData) ?
-					ChartsWidget::widget([
-						'type' => 'donut',
-						'id' => 'chart-leads-statuses-count' . $searchModel->getUniqueId(),
-						'series' => $statusData['series'],
-						'options' => [
-							'colors' => $statusData['colors'],
-							'labels' => $statusData['labels'],
-							'title' => [
-								'text' => Yii::t('lead', 'Status Count'),
-								'align' => 'center',
-							],
-							'legend' => [
-								//		'position' => 'bottom',
-								'width' => 200,
-								'formatter' => new JsExpression('function(seriesName, opts){
+			<?= !empty($statusData) ?
+				ChartsWidget::widget([
+					'type' => 'donut',
+					'containerOptions' => [
+						'class' => 'col-sm-12 col-md-4',
+						'style' => [
+							'height' => '50vh',
+						],
+					],
+					'id' => 'chart-leads-statuses-count' . $searchModel->getUniqueId(),
+					'series' => $statusData['series'],
+					'options' => [
+						'colors' => $statusData['colors'],
+						'labels' => $statusData['labels'],
+						'title' => [
+							'text' => Yii::t('lead', 'Status Count'),
+							'align' => 'center',
+						],
+						'legend' => [
+							//		'position' => 'bottom',
+							'width' => 200,
+							'formatter' => new JsExpression('function(seriesName, opts){
 															return [seriesName, " - ", opts.w.globals.series[opts.seriesIndex]];
 															}'),
-							],
-							'plotOptions' => [
-								'pie' => [
-									'donut' => [
-										'labels' => [
+						],
+						'plotOptions' => [
+							'pie' => [
+								'donut' => [
+									'labels' => [
+										'show' => true,
+										'total' => [
 											'show' => true,
-											'total' => [
-												'show' => true,
-												'showAlways' => true,
-												'label' => Yii::t('common', 'Sum'),
-											],
+											'showAlways' => true,
+											'label' => Yii::t('common', 'Sum'),
 										],
 									],
 								],
 							],
 						],
-					])
-					: ''
-				?>
-			</div>
+					],
+				])
+				: ''
+			?>
 
-			<div class="col-sm-12 col-md-4" style="height:50vh">
-				<?= !empty($typesData) ?
-					ChartsWidget::widget([
-						'type' => ChartsWidget::TYPE_DONUT,
-						'id' => 'chart-leads-types-count' . $searchModel->getUniqueId(),
-						'series' => $typesData['series'],
-						'options' => [
-							'labels' => $typesData['labels'],
-							'title' => [
-								'text' => Yii::t('lead', 'Types Count'),
-								'align' => 'center',
-							],
-							'legend' => [
-								//		'position' => 'bottom',
-								'width' => 200,
-								'formatter' => new JsExpression('function(seriesName, opts){
+			<?= !empty($typesData) ?
+				ChartsWidget::widget([
+					'type' => ChartsWidget::TYPE_DONUT,
+					'containerOptions' => [
+						'class' => 'col-sm-12 col-md-4',
+						'style' => [
+							'height' => '50vh',
+						],
+					],
+					'id' => 'chart-leads-types-count' . $searchModel->getUniqueId(),
+					'series' => $typesData['series'],
+					'options' => [
+						'labels' => $typesData['labels'],
+						'title' => [
+							'text' => Yii::t('lead', 'Types Count'),
+							'align' => 'center',
+						],
+						'legend' => [
+							//		'position' => 'bottom',
+							'width' => 200,
+							'formatter' => new JsExpression('function(seriesName, opts){
 															return [seriesName, " - ", opts.w.globals.series[opts.seriesIndex]];
 															}'),
-							],
-							'plotOptions' => [
-								'pie' => [
-									'donut' => [
-										'labels' => [
+						],
+						'plotOptions' => [
+							'pie' => [
+								'donut' => [
+									'labels' => [
+										'show' => true,
+										'total' => [
 											'show' => true,
-											'total' => [
-												'show' => true,
-												'showAlways' => true,
-												'label' => Yii::t('common', 'Sum'),
-											],
+											'showAlways' => true,
+											'label' => Yii::t('common', 'Sum'),
 										],
 									],
 								],
 							],
 						],
-					])
-					: ''
-				?>
-			</div>
+					],
+				])
+				: ''
+			?>
 
-			<div class="col-sm-12 col-md-4" style="height:50vh">
-				<?= !empty($sourcesData) ?
-					ChartsWidget::widget([
-						'type' => 'donut',
-						'id' => 'chart-leads-sources-count' . $searchModel->getUniqueId(),
-						'series' => $sourcesData['series'],
-						'options' => [
-							'labels' => $sourcesData['labels'],
-							'title' => [
-								'text' => Yii::t('lead', 'Sources Count'),
-								'align' => 'center',
-							],
-							'legend' => [
-								//		'position' => 'bottom',
-								'width' => 200,
-								'formatter' => new JsExpression('function(seriesName, opts){
+
+			<?= !empty($sourcesData) ?
+				ChartsWidget::widget([
+					'type' => ChartsWidget::TYPE_DONUT,
+					'containerOptions' => [
+						'class' => 'col-sm-12 col-md-4',
+						'style' => [
+							'height' => '50vh',
+						],
+					],
+					'id' => 'chart-leads-sources-count' . $searchModel->getUniqueId(),
+					'series' => $sourcesData['series'],
+					'options' => [
+						'labels' => $sourcesData['labels'],
+						'title' => [
+							'text' => Yii::t('lead', 'Sources Count'),
+							'align' => 'center',
+						],
+						'legend' => [
+							//		'position' => 'bottom',
+							'width' => 200,
+							'formatter' => new JsExpression('function(seriesName, opts){
 															return [seriesName, " - ", opts.w.globals.series[opts.seriesIndex]];
 															}'),
-							],
-							'plotOptions' => [
-								'pie' => [
-									'donut' => [
-										'labels' => [
+						],
+						'plotOptions' => [
+							'pie' => [
+								'donut' => [
+									'labels' => [
+										'show' => true,
+										'total' => [
 											'show' => true,
-											'total' => [
-												'show' => true,
-												'showAlways' => true,
-												'label' => Yii::t('common', 'Sum'),
-											],
+											'showAlways' => true,
+											'label' => Yii::t('common', 'Sum'),
 										],
 									],
 								],
 							],
 						],
-					])
-					: ''
-				?>
-			</div>
+					],
+				])
+				: ''
+			?>
 		</div>
 
 		<?php if (!empty($usersData)): ?>
@@ -275,6 +306,14 @@ foreach ($leadsCountSeries as $index => $data) {
 					[
 						'name' => Yii::t('lead', 'Users'),
 						'data' => $usersData['series'],
+					],
+				],
+				'chart' => [
+					'stacked' => true,
+					'zoom' => [
+						'enabled' => true,
+						'type' => 'x',
+						'autoScaleYaxis' => true,
 					],
 				],
 				'options' => [
@@ -300,9 +339,11 @@ foreach ($leadsCountSeries as $index => $data) {
 <?php endif; ?>
 	<?php if (!empty($usersData)): ?>
 		<div class="row">
-			<div class="col-sm-12 col-md-6" style="height:30vh">
+			<div class="col-sm-12 col-md-6">
 				<?= ChartsWidget::widget([
 					'type' => ChartsWidget::TYPE_DONUT,
+					'height' => '300',
+
 					'id' => 'chart-leads-users-count' . $searchModel->getUniqueId(),
 					'series' => $usersData['series'],
 					'options' => [
