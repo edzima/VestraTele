aafa627aeb7ae856b0c70eadec7f3082c2b2c397
edzima <fachowosc@gmail.com>
2024-06-14 13:07:53 +0200

    fix: restore render grid selection buttons in index page (pjax fix).

--- backend/modules/issue/views/issue/index.php
+++ backend/modules/issue/views/issue/index.php
@@ -1,7 +1,9 @@
 <?php
 
+use backend\helpers\Html;
 use backend\modules\issue\models\search\IssueSearch;
 use common\behaviors\IssueTypeParentIdAction;
+use common\models\user\Worker;
 use common\widgets\grid\SelectionForm;
 use yii\data\ActiveDataProvider;
 use yii\widgets\Pjax;
@@ -35,12 +37,152 @@ $this->params['issueParentTypeNav'] = [
 
 	<?= $this->render('_search', ['model' => $searchModel]) ?>
 
-	<?= $this->render('_grid-selection-buttons', [
-		'searchModel' => $searchModel,
-		'dataProvider' => $dataProvider,
-		'gridId' => 'issues-list',
-	]) ?>
+	<div class="grid-selection-links-wrapper">
 
+
+		<?php
+
+		SelectionForm::begin([
+			'formWrapperSelector' => '.selection-form-wrapper',
+			'gridId' => 'issues-list',
+		]);
+		?>
+
+		<p class="selection-form-wrapper hidden">
+
+			<?php if (Yii::$app->user->can(Worker::PERMISSION_MULTIPLE_SMS)): ?>
+				<span class="btn-group">
+				<?= Html::submitButton(
+					Html::faicon('envelope')
+					,
+					[
+						'class' => 'btn btn-success',
+						'name' => 'route',
+						'value' => 'sms/push-multiple',
+						'data-pjax' => '0',
+						'title' => Yii::t('backend', 'Send SMS'),
+						'aria-label' => Yii::t('backend', 'Send SMS'),
+					])
+				?>
+
+				<?=
+				!empty($dataProvider->getModels())
+				&& $dataProvider->pagination->pageCount > 1
+					? Html::a(
+					count($searchModel->getAllIds($dataProvider->query)), [
+					'sms/push-multiple',
+				],
+					[
+						'data' => [
+							'pjax' => '0',
+							'method' => 'POST',
+							'params' => [
+								'ids' => $searchModel->getAllIds($dataProvider->query),
+							],
+						],
+						'class' => 'btn btn-success',
+						'title' => Yii::t('backend', 'Send SMS: {count}', [
+							'count' => count($searchModel->getAllIds($dataProvider->query)),
+						]),
+						'aria-label' => Yii::t('backend', 'Send SMS: {count}', [
+							'count' => count($searchModel->getAllIds($dataProvider->query)),
+						]),
+					]
+				)
+					: ''
+				?>
+			</span>
+
+			<?php endif; ?>
+
+
+			<?php if (Yii::$app->user->can(Worker::PERMISSION_ISSUE_TYPE_MANAGER)): ?>
+
+				<span class="btn-group">
+				<?= Html::submitButton(
+					'<i class="fa fa-sitemap"></i>',
+					[
+						'class' => 'btn btn-info',
+						'name' => 'route',
+						'value' => 'type/update-multiple',
+						'data-pjax' => '0',
+						'title' => Yii::t('issue', 'Update Type'),
+						'aria-label' => Yii::t('issue', 'Update Type'),
+					])
+				?>
+				<?= !empty($dataProvider->getModels())
+				&& $dataProvider->pagination->pageCount > 1
+					? Html::a(count($searchModel->getAllIds($dataProvider->query)), [
+						'type/update-multiple',
+					],
+						[
+							'data' => [
+								'pjax' => '0',
+								'method' => 'POST',
+								'params' => [
+									'ids' => $searchModel->getAllIds($dataProvider->query),
+								],
+							],
+							'class' => 'btn btn-info',
+							'title' => Yii::t('issue', 'Update Type: {count}', [
+								'count' => count($searchModel->getAllIds($dataProvider->query)),
+							]),
+							'aria-label' => Yii::t('issue', 'Update Type: {count}', [
+								'count' => count($searchModel->getAllIds($dataProvider->query)),
+							]),
+						]
+					)
+					: ''
+				?>
+			</span>
+
+			<?php endif; ?>
+
+			<?php if (Yii::$app->user->can(Worker::PERMISSION_ISSUE_TAG_MANAGER)): ?>
+
+				<span class="btn-group">
+				<?= Html::submitButton(
+					Html::icon('tag'),
+					[
+						'class' => 'btn btn-success',
+						'name' => 'route',
+						'value' => 'tag/link-multiple',
+						'data-pjax' => '0',
+						'title' => Yii::t('issue', 'Tags'),
+						'aria-label' => Yii::t('issue', 'Tags'),
+					])
+				?>
+				<?= !empty($dataProvider->getModels())
+				&& $dataProvider->pagination->pageCount > 1
+					? Html::a(count($searchModel->getAllIds($dataProvider->query)), [
+						'tag/link-multiple',
+					],
+						[
+							'data' => [
+								'pjax' => '0',
+								'method' => 'POST',
+								'params' => [
+									'ids' => $searchModel->getAllIds($dataProvider->query),
+								],
+							],
+							'class' => 'btn btn-success',
+							'title' => Yii::t('issue', 'Link Tags: {count}', [
+								'count' => count($searchModel->getAllIds($dataProvider->query)),
+							]),
+							'aria-label' => Yii::t('issue', 'Link Tags: {count}', [
+								'count' => count($searchModel->getAllIds($dataProvider->query)),
+							]),
+						]
+					)
+					: ''
+				?>
+			</span>
+
+			<?php endif; ?>
+
+
+		</p>
+	</div>
 
 
 	<?= $this->render('_grid', [
