d3f68f158b7a3fd6be4cb578e7db6d1ee07c638c
edzima <fachowosc@gmail.com>
2024-07-22 13:49:31 +0200

    feat: primary link change to lead from report for lead push email.

--- common/mail/leadPush-html.php
+++ common/mail/leadPush-html.php
@@ -11,17 +11,10 @@ $this->title = Yii::t('lead', 'New Lead: {lead}', [
 	'lead' => $lead->getName(),
 ]);
 
-$reportLink = FrontendUrl::toRoute(
-	[
-		'/lead/report/report',
-		'id' => $lead->getId(),
-		'hash' => $lead->getHash(),
-	],
-	true
-);
+$leadLink = FrontendUrl::leadView($lead->getId(), true);
 
-$this->params['primaryButtonText'] = Yii::t('lead', 'Create Lead Report');
-$this->params['primaryButtonHref'] = $reportLink;
+$this->params['primaryButtonText'] = Yii::t('lead', 'Details');
+$this->params['primaryButtonHref'] = $leadLink;
 ?>
 <div class="lead-push-email">
 	<p><?= Yii::t('lead', 'Source') ?>: <?= Html::encode($lead->getSource()->getName()) ?></p>
