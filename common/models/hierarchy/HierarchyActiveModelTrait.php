65da1514476cec5df75460329006c4cdcd5a040c
edzima <fachowosc@gmail.com>
2024-08-06 11:19:13 +0200

    feat: create hierarchy trait for active model. feat: add internal cache for tree in hierarchy component.

---
+++ common/models/hierarchy/HierarchyActiveModelTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace common\models\hierarchy;
+
+use common\components\HierarchyComponent;
+use yii\db\ActiveQuery;
+
+trait HierarchyActiveModelTrait {
+
+	private static ?HierarchyComponent $hierarchy = null;
+
+	public static function getHierarchy(): HierarchyComponent {
+		if (static::$hierarchy === null) {
+			$hierarchy = new HierarchyComponent();
+			$hierarchy->modelClass = static::class;
+			static::$hierarchy = $hierarchy;
+		}
+		return static::$hierarchy;
+	}
+
+	public function getParentsIds(): array {
+		if ($this->getParentId() === null) {
+			return [];
+		}
+		return static::getHierarchy()->getParentsIds($this->{$this->hierarchyKeyAttribute()});
+	}
+
+	public function getParentId(): ?int {
+		return $this->{$this->hierarchyParentAttribute()};
+	}
+
+	public function getChildesIds(): array {
+		return static::getHierarchy()->getChildesIds($this->{$this->hierarchyKeyAttribute()});
+	}
+
+	public function getAllChildesIds(): array {
+		return static::getHierarchy()->getAllChildesIds($this->{$this->hierarchyKeyAttribute()});
+	}
+
+	public function getParentsQuery(): ActiveQuery {
+		return static::find()->where([
+			$this->hierarchyKeyAttribute() => $this->getParentsIds(),
+		]);
+	}
+
+	protected function hierarchyKeyAttribute(): string {
+		return 'id';
+	}
+
+	protected function hierarchyParentAttribute(): string {
+		return 'parent_id';
+	}
+
+}
