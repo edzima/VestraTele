b9e917fdb5d12d9efef9fc32e461404eab3a7159
edzima <fachowosc@gmail.com>
2024-08-14 12:02:05 +0200

    feat: rewrite CostComponent to recalculte lead value from date range.

---
+++ common/modules/lead/models/CampaignCost.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace common\modules\lead\models;
+
+use yii\base\Model;
+use yii\db\Query;
+
+class CampaignCost extends Model {
+
+	public ?string $fromAt = null;
+	public ?string $toAt = null;
+
+	public int $campaign_id;
+
+	public int $leads_count;
+
+	public ?float $sum;
+	public ?float $single_cost_value;
+	public array $leads_ids = [];
+
+	public static function getModels(
+		?string $fromAt,
+		?string $toAt,
+		array $campaignsIds = []) {
+
+		$subQuery = LeadCost::find()
+			->between($fromAt, $toAt)
+			->andFilterWhere([
+				LeadCost::tableName() . '.campaign_id' => $campaignsIds,
+			]);
+
+		$subQuery->select([
+			'sum(value) as sum',
+			'campaign_id',
+		]);
+		$subQuery->groupBy([
+			LeadCost::tableName() . '.campaign_id',
+		]);
+		$query = new Query();
+		$query->select([
+			'c.sum',
+			'c.campaign_id',
+			'count(l.id) as leads_count',
+			'c.sum/count(l.id) as single_cost_value',
+			'GROUP_CONCAT(l.id) as leads_ids',
+		]);
+		$query->from(['c' => $subQuery]);
+		$query->leftJoin(['l' => Lead::tableName()], 'l.campaign_id = c.campaign_id');
+		$query->andWhere([
+			'between',
+			'l.date_at',
+			date('Y-m-d 00:00:00', strtotime($fromAt)),
+			date('Y-m-d 23:59:59', strtotime($toAt)),
+		]);
+		$query->groupBy('c.campaign_id');
+		$data = $query->all();
+		$models = [];
+		foreach ($data as $datum) {
+			$model = new static([
+				'fromAt' => $fromAt,
+				'toAt' => $toAt,
+				'sum' => $datum['sum'],
+				'campaign_id' => $datum['campaign_id'],
+				'single_cost_value' => $datum['single_cost_value'],
+				'leads_ids' => $datum['leads_ids']
+					? explode(',', $datum['leads_ids'])
+					: [],
+			]);
+			$models[] = $model;
+		}
+		return $models;
+	}
+
+}
