2320ed65215f9be2aab2cf16ee78448ec412f5ff
edzima <fachowosc@gmail.com>
2024-08-28 15:46:26 +0200

    feat: create LeadUserStastusChart widget.

---
+++ common/modules/lead/widgets/chart/views/lead-user-status.php
@@ -0,0 +1,27 @@
+<?php
+
+/* @var $nav string */
+/* @var $donutUsersChart string */
+/* @var $areaUsersStatusChart string */
+/* @var $donutStatusesChart string */
+?>
+
+<div class="lead-user-status-chart-wrapper">
+	<?= $nav ?>
+	<br>
+	<div class="row">
+		<?php if (!empty($donutUsersChart)): ?>
+			<div class="col-sm-12 col-md-7 col-lg-8">
+				<?= $areaUsersStatusChart ?>
+			</div>
+			<div class="col-sm-12 col-md-5 col-lg-4">
+				<?= $donutUsersChart ?>
+			</div>
+		<?php else: ?>
+			<div class="col-sm-12">
+				<?= $areaUsersStatusChart ?>
+			</div>
+		<?php endif; ?>
+
+	</div>
+</div>
