398d13f25b827e4bcb2818715eea2e9bfc805984
edzima <fachowosc@gmail.com>
2024-10-07 13:32:20 +0200

    fix: bind userId to backend summon view issue search.

--- backend/modules/entityResponsible/controllers/DefaultController.php
+++ backend/modules/entityResponsible/controllers/DefaultController.php
@@ -56,6 +56,7 @@ class DefaultController extends Controller {
 		$model = $this->findModel($id);
 		$searchModel = new IssueSearch();
 		$searchModel->entity_responsible_id = $id;
+		$searchModel->userId = Yii::$app->user->getId();
 		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 		return $this->render('view', [
 			'model' => $model,
