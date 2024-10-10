d85a92443e5f59214eb4a7fdff27671b9f9f1aca
edzima <fachowosc@gmail.com>
2024-10-10 12:23:52 +0200

    test: more test for ModelAccessManager.

--- common/components/rbac/SettlementTypeAccessManager.php
+++ common/components/rbac/SettlementTypeAccessManager.php
@@ -2,14 +2,11 @@
 
 namespace common\components\rbac;
 
-use common\models\settlement\SettlementType;
 use common\models\user\Worker;
 
 class SettlementTypeAccessManager extends ModelAccessManager {
 
 	public const ACTION_ISSUE_VIEW = 'issue-view';
-
-	public string $modelClass = SettlementType::class;
 
 	public function getActions(): array {
 		return array_merge(parent::getActions(), [
