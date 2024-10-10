4621afec44a2ea3cd45e229579c90ea78bd87ca9
edzima <fachowosc@gmail.com>
2024-10-10 13:42:09 +0200

    feat: add background color for appeal lawsuit.

--- common/modules/calendar/models/LawsuitEvent.php
+++ common/modules/calendar/models/LawsuitEvent.php
@@ -7,6 +7,7 @@ use common\modules\court\models\Lawsuit;
 
 class LawsuitEvent extends FullCalendarEvent {
 
+	public const BACKGROUND_APPEAL = '#df2424';
 	private Lawsuit $model;
 
 	public string $courtType;
@@ -19,6 +20,14 @@ class LawsuitEvent extends FullCalendarEvent {
 		$this->courtType = $model->court->type;
 		$this->start = $model->due_at;
 		$this->url = Url::to(['/court/lawsuit/view', 'id' => $model->id]);
+		$this->backgroundColor = $this->getBackgroundColor();
+	}
+
+	protected function getBackgroundColor(): ?string {
+		if ($this->model->court->isAppeal()) {
+			return static::BACKGROUND_APPEAL;
+		}
+		return null;
 	}
 
 	protected function getTitle(): string {
