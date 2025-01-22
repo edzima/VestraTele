20009a53a24030866813a56bc112a8f4b3464559
edzima <fachowosc@gmail.com>
2025-01-22 13:55:01 +0100

    chown: rwd for phone lead view form.

--- common/modules/lead/views/lead/phone.php
+++ common/modules/lead/views/lead/phone.php
@@ -20,7 +20,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="lead-phone-view">
 
-	<h1><?= Html::encode($this->title) ?></h1>
 	<p>
 		<?= !empty($model->phone) ? CreateLeadBtnWidget::widget([
 			'owner_id' => Yii::$app->user->getId(),
@@ -30,32 +29,45 @@ $this->params['breadcrumbs'][] = $this->title;
 
 	<div class="lead-phone-search">
 
-		<?php $form = ActiveForm::begin([
-			'action' => ['phone'],
-			'method' => 'get',
-		]); ?>
+		<div class="row">
+			<div class="col-xs-12 col-sm-5 col-md-4 col-lg-2">
 
-		<?= $form->field($model, 'phone')->textInput() ?>
 
-		<div class="form-group">
-			<?= Html::submitButton(Yii::t('lead', 'Search'), ['class' => 'btn btn-primary']) ?>
+				<?php $form = ActiveForm::begin([
+					'action' => ['phone'],
+					'method' => 'get',
+				]); ?>
+
+				<?= $form->field($model, 'phone')->textInput() ?>
+
+				<div class="form-group">
+					<?= Html::submitButton(Yii::t('lead', 'Search'), ['class' => 'btn btn-primary']) ?>
+				</div>
+
+				<?php ActiveForm::end(); ?>
+			</div>
 		</div>
 
-		<?php ActiveForm::end(); ?>
+
 	</div>
 
 	<div class="lead-phone-models">
-		<?= SameContactsListWidget::widget([
-			'withReportBtn' => false,
-			'dataProvider' => $dataProvider,
-			'withType' => true,
-			'withHeader' => false,
-			'summaryOptions' => [
-				'class' => 'col-md-12',
-			],
-			'itemOptions' => [
-				'class' => 'col-md-6',
-			],
-		]) ?>
+		<div class="row">
+
+			<?= SameContactsListWidget::widget([
+				'withReportBtn' => false,
+				'dataProvider' => $dataProvider,
+				'withType' => true,
+				'withHeader' => false,
+				'summaryOptions' => [
+					'class' => 'col-md-12',
+				],
+				'itemOptions' => [
+					'class' => 'col-md-6',
+				],
+			]) ?>
+
+
+		</div>
 	</div>
 </div>
