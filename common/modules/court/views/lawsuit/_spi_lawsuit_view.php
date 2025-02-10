97d956123c7eb8e39817b87c95dac94b6094ba50
edzima <fachowosc@gmail.com>
2025-02-10 11:26:09 +0100

    feat: session spi details only visible for admin.

--- common/modules/court/views/lawsuit/_spi_lawsuit_view.php
+++ common/modules/court/views/lawsuit/_spi_lawsuit_view.php
@@ -1,6 +1,7 @@
 <?php
 
 use common\helpers\Url;
+use common\models\user\User;
 use common\modules\court\modules\spi\entity\lawsuit\LawsuitViewIntegratorDto;
 use common\modules\court\modules\spi\Module;
 use kartik\tabs\TabsX;
@@ -11,89 +12,93 @@ use yii\widgets\DetailView;
 /** @var LawsuitViewIntegratorDto $model */
 /** @var DataProviderInterface $parties |null */
 
+$items = [
+
+	[
+		'label' => Module::t('lawsuit', 'Lawsuit'),
+		'content' => DetailView::widget([
+			'model' => $model,
+			'attributes' => [
+				[
+					'attribute' => 'result',
+					'visible' => !empty($model->result),
+				],
+				'subject',
+				[
+					'attribute' => 'description',
+					'format' => 'ntext',
+					'visible' => !empty($model->description),
+				],
+				'receiptDate:datetime',
+				[
+					'attribute' => 'finishDate',
+					'format' => 'date',
+					'visible' => !empty($model->finishDate),
+				],
+
+				'departmentName',
+				'judgeName',
+				[
+					'attribute' => 'value',
+					'visible' => !empty($model->value),
+				],
+			],
+		]),
+	],
+	[
+		'label' => Module::t('lawsuit', 'Parties'),
+		'linkOptions' => [
+			'data-url' => Url::to([
+				'spi/lawsuit/parties',
+				'id' => $model->id,
+				'appeal' => $this->params['appeal'],
+			]),
+		],
+		//				'content' => GridView::widget([
+		//					'dataProvider' => new ArrayDataProvider([
+		//						'allModels' => $model->getLawsuitParties(),
+		//						'modelClass' => LawsuitPartyDTO::class,
+		//					]),
+		//				]),
+	],
+	[
+		'label' => Module::t('document', 'Documents'),
+		'linkOptions' => [
+			'data-url' => Url::to([
+				'spi/document/lawsuit',
+				'id' => $model->id,
+				'appeal' => $this->params['appeal'],
+			]),
+		],
+	],
+	[
+		'label' => Module::t('lawsuit', 'Proceedings'),
+		'linkOptions' => [
+			'data-url' => Url::to([
+				'spi/lawsuit/proceedings',
+				'id' => $model->id,
+				'appeal' => $this->params['appeal'],
+			]),
+		],
+	],
+];
+if (Yii::$app->user->can(User::ROLE_ADMINISTRATOR)) {
+	$items[] = [
+		'label' => Module::t('lawsuit', 'Sessions'),
+		'linkOptions' => [
+			'data-url' => Url::to([
+				'spi/lawsuit/sessions',
+				'id' => $model->id,
+				'appeal' => $this->params['appeal'],
+			]),
+		],
+	];
+}
 ?>
 <div class="court-lawsuit-spi-details-view">
 
 	<?= TabsX::widget([
-		'items' => [
-			[
-				'label' => Module::t('lawsuit', 'Lawsuit'),
-				'content' => DetailView::widget([
-					'model' => $model,
-					'attributes' => [
-						[
-							'attribute' => 'result',
-							'visible' => !empty($model->result),
-						],
-						'subject',
-						[
-							'attribute' => 'description',
-							'format' => 'ntext',
-							'visible' => !empty($model->description),
-						],
-						'receiptDate:datetime',
-						[
-							'attribute' => 'finishDate',
-							'format' => 'date',
-							'visible' => !empty($model->finishDate),
-						],
-
-						'departmentName',
-						'judgeName',
-						[
-							'attribute' => 'value',
-							'visible' => !empty($model->value),
-						],
-					],
-				]),
-			],
-			[
-				'label' => Module::t('lawsuit', 'Parties'),
-				'linkOptions' => [
-					'data-url' => Url::to([
-						'spi/lawsuit/parties',
-						'id' => $model->id,
-						'appeal' => $this->params['appeal'],
-					]),
-				],
-				//				'content' => GridView::widget([
-				//					'dataProvider' => new ArrayDataProvider([
-				//						'allModels' => $model->getLawsuitParties(),
-				//						'modelClass' => LawsuitPartyDTO::class,
-				//					]),
-				//				]),
-			],
-			[
-				'label' => Module::t('document', 'Documents'),
-				'linkOptions' => [
-					'data-url' => Url::to([
-						'spi/document/lawsuit',
-						'id' => $model->id,
-						'appeal' => $this->params['appeal'],
-					]),
-				],
-			],
-			[
-				'label' => Module::t('lawsuit', 'Proceedings'),
-				'linkOptions' => [
-					'data-url' => Url::to([
-						'spi/lawsuit/proceedings',
-						'id' => $model->id,
-						'appeal' => $this->params['appeal'],
-					]),
-				],
-			],
-			[
-				'label' => Module::t('lawsuit', 'Sessions'),
-				'linkOptions' => [
-					'data-url' => Url::to([
-						'spi/lawsuit/sessions',
-						'id' => $model->id,
-						'appeal' => $this->params['appeal'],
-					]),
-				],
-			],
-		],
+		'items' => $items,
 	]) ?>
 
 </div>
